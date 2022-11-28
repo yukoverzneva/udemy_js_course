45690d026132de4ac6740fd612fb7c74f223d045
Julia Koverzneva <106422712+yukoverzneva@users.noreply.github.com>
2022-11-28 13:30:01 +0300

    add lazy loading images

--- JS GitHub/13/script.js
+++ JS GitHub/13/script.js
@@ -172,6 +172,26 @@ allSections.forEach(function (section) {
 });
 
 //Lazy Loading Images
+const imgTargets = document.querySelectorAll('img[data-src]');
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  //Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  //Remove blurry filter
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  // rootMargin: '-200px',
+  rootMargin: '200px',
+});
+imgTargets.forEach(img => imgObserver.observe(img));
 
 /*/////////////////////////////////////////////////////
 LECTURES
